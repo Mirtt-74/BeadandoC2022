5c1485de53526bf2bd524e3c2645736b9d434e94
mirtt <mirtt@DESKTOP-GNEG2GC>
2022-03-06 19:13:02 +0100

    Add project files.

---
+++ Beadando2022/Fajlkezelo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Beadando2022
+{
+    interface IFajl
+    {
+        void FajlBeolvasas(string s);
+    }
+
+    class Adatok
+    {
+        public string tanarneve;
+        public DateTime ido;
+        public DateTime foglalasideje;
+
+    }
+
+    class AdatFeldolgozas:IFajl
+    {
+        public List<Adatok> adatok = new List<Adatok>();
+
+        public AdatFeldolgozas() 
+        {
+          
+        }
+
+        //public bool Vanilyentanar(string nev)
+        //{
+        //    bool volt = false;
+        //    int i = 0;
+
+        //    while (nev != adatok[i].tanarneve && i < AdatokSzama() - 1)
+        //    {
+        //        i++;
+        //    }
+
+        //    if (i < AdatokSzama() - 1)
+        //    {
+        //        volt = true;
+        //    }
+        //    return volt;
+        //}
+
+        public string TanarKeres(string nev)
+        {
+            int db = 0;
+            string ki;
+   
+            for (int i = 0; i < AdatokSzama() - 1; i++)
+            {
+                if (nev == adatok[i].tanarneve) db++;
+            }
+
+            if (db == 0)
+            {
+                ki = "A megadott néven nincs időpontfoglalás";
+            }
+            else
+            {
+                ki = nev + " néven " + db.ToString() + " időpontfoglalás van.";
+            }
+            return ki;
+        }
+
+        public int AdatokSzama()
+        {
+            return adatok.Count;
+        }
+
+        public void FajlBeolvasas(string fajlnev)
+        {
+            string sor;
+
+            using (StreamReader f = new StreamReader(fajlnev))
+            {
+                while (!f.EndOfStream)
+                {
+                    sor = f.ReadLine();
+                    var szo = sor.Split(' ');
+                    //int len = adatok.Count;
+
+                    Adatok adat = new Adatok();
+
+                    adat.tanarneve = szo[0] + " " + szo[1];
+                    adat.ido = DateTime.Parse(szo[2]);
+                    szo[3] = szo[3].Replace("-", " ");
+                    adat.foglalasideje = DateTime.Parse(szo[3]);
+
+                    adatok.Add(adat);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Beadando2022/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fajlnev = "fogado.txt";
+            bool vanilyen;
+
+            AdatFeldolgozas fs = new AdatFeldolgozas();
+
+            Console.WriteLine("1. feladat: fájl beolvasás és adattárolás");
+
+            fs.FajlBeolvasas(fajlnev);
+
+            Console.WriteLine("\n2. feladat");
+            Console.WriteLine("Foglalások száma: {0}", fs.AdatokSzama());
+
+            Console.WriteLine("\n3. feladat");
+            Console.Write("Adjon meg egy nevet: ");
+            string benev = Console.ReadLine();
+            Console.Write(fs.TanarKeres(benev));
+
+            //do
+            //{
+            //    Console.Write("Adjon meg egy nevet: ");
+            //    string benev = Console.ReadLine();
+            //    vanilyen = fs.TanarKeres(benev);
+            //    if (vanilyen == false) Console.WriteLine("\nNincs ilyen nevű tanár, adja meg újra a nevet!");
+            //} while (vanilyen);
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
